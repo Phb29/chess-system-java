781e38024811dff6317a544de267937231c22885
Phb29 <paulototototo@gmail.com>
2021-03-27 20:54:39 -0300

    defensive board

--- src/boardgame/board.java
+++ src/boardgame/board.java
@@ -5,6 +5,9 @@ public class board {
 	private int columns;
 	private piece[][] Piece;
 	public board(int rows, int columns) {
+		if(rows <1 ||columns <1) {
+			throw  new  BoardException("erro boarning");
+		}
 		
 		this.rows = rows;
 		this.columns = columns;
@@ -13,25 +16,44 @@ public class board {
 	public int getRows() {
 		return rows;
 	}
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
+	
 	public int getColumns() {
 		return columns;
 	}
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
+	
 	public piece Piece(int row,int column) {
+		if(!positionExists(row,column)) {
+			throw  new BoardException("position not on the board");
+		}
 		return Piece[row][column];
 	}
 	public piece Piece(position Position) {
+		if(!positionExists(Position)) {
+			throw  new BoardException("position not on the board");
+		}
 		return Piece[Position.getRow()][Position.getColumn()];	
 		              }
 	public void placepiece(piece Pie, position Posi) {
+		if(thereiseapiece(Posi)) {
+			throw new BoardException("there is already a piece on position"+Posi);
+		}
 		Piece [Posi.getRow()][Posi.getColumn()]=Pie;
 		Pie.Position=Posi;
 		
+	}
+	private boolean  positionExists(int row,int column) {
+		return row>=0 && row < rows && column>=0 && column<columns;
+		
+	}
+	public  boolean positionExists(position Position) {
+		return positionExists(Position.getRow(),Position.getColumn());	
+		
+	}
+	public boolean thereiseapiece(position Position) {
+		if(!positionExists(Position)) {
+			throw  new BoardException("position not on the board");
+		}
+		return Piece(Position)!=null;
 	}
 
 }
